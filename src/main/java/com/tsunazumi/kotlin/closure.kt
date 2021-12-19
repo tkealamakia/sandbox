6101a6185095f50106855ac27a0e3268688ef449
Tom Kealamakia <tkealamakia@overstock.com>
2021-12-18 17:45:10 -0700

    Adding kotlin support in maven

---
+++ src/main/java/com/tsunazumi/kotlin/closure.kt
@@ -0,0 +1,11 @@
+package com.tsunazumi.kotlin
+
+fun main(args: Array<String>) {
+  var factor = 2
+  val doubled = listOf(1,2).map { it * factor }
+  val doubledAlso = sequenceOf(1,2).map { it * factor }
+
+//  factor = 0
+//  doubled.forEach { println(it) }
+  doubledAlso.forEach { println(it) }
+}
