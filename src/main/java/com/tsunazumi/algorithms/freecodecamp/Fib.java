05de940c46d4cadfe228a1e3f9439a40fee7bca3
Tom Kealamakia <tkealamakia@gmail.com>
2022-01-16 05:36:12 -0700

    Adding files

---
+++ src/main/java/com/tsunazumi/algorithms/freecodecamp/Fib.java
@@ -0,0 +1,18 @@
+package com.tsunazumi.algorithms.freecodecamp;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Fib {
+  public static void main(String[] args) {
+    Map<Long, Long> memo = new HashMap<>();
+    System.out.println(fib(89, memo));
+  }
+
+  public static long fib(long n, Map<Long, Long> map) {
+    if (map.containsKey(n)) return map.get(n);
+    if (n <= 2) return 1;
+    map.put(n, fib(n-1,map) + fib(n-2,map));
+    return map.get(n);
+  }
+}
