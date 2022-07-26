72f9e4f92dbfe4b55abe0880cfecf4d0eb58e00d
Tom Kealamakia <tkealamakia@gmail.com>
2022-07-25 21:43:52 -0600

    Changes

--- src/main/java/com/tsunazumi/buchalka/concurrency/producerconsumer/Main.java
+++ src/main/java/com/tsunazumi/buchalka/concurrency/producerconsumer/Main.java
@@ -6,6 +6,7 @@ import java.io.EOFException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.*;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
@@ -14,13 +15,35 @@ public class Main {
   public static void main(String[] args) throws Exception {
     List<String> buffer = new ArrayList<>();
     ReentrantLock bufferLock = new ReentrantLock();
+
+    ExecutorService executorService = Executors.newFixedThreadPool(3);
+
     MyProducer producer = new MyProducer(buffer, ThreadColor.ANSI_GREEN, bufferLock);
     MyConsumer consumer1 = new MyConsumer(buffer, ThreadColor.ANSI_PURPLE, bufferLock);
     MyConsumer consumer2 = new MyConsumer(buffer, ThreadColor.ANSI_CYAN, bufferLock);
 
-    new Thread(producer).start();
-    new Thread(consumer1).start();
-    new Thread(consumer2).start();
+
+    Future<String> future = executorService.submit(new Callable<String>() {
+      @Override
+      public String call() throws Exception {
+        Thread.sleep(2000);
+        System.out.println(ThreadColor.ANSI_RED);
+        return "This is the callable result";
+      }
+    });
+    executorService.execute(producer);
+    executorService.execute(consumer1);
+    executorService.execute(consumer2);
+
+    try {
+      System.out.println(future.get());
+    } catch(ExecutionException e) {
+      System.out.println("Something went wrong");
+    } catch(InterruptedException e) {
+      System.out.println("Thread running the task was interrupted.");
+    }
+
+    executorService.shutdown();
 
   }
 }
