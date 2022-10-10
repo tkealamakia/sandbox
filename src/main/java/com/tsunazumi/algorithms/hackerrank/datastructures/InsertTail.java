be8de25d36760d59a69c4d4e6f10f070eb6b0cc5
Tom Kealamakia <tkealamakia@gmail.com>
2022-10-10 01:06:51 -0600

    More stuff

--- src/main/java/com/tsunazumi/algorithms/hackerrank/datastructures/InsertTail.java
+++ src/main/java/com/tsunazumi/algorithms/hackerrank/datastructures/InsertTail.java
@@ -3,6 +3,10 @@ package com.tsunazumi.algorithms.hackerrank.datastructures;
 class SinglyLinkedListNode {
   int data;
   SinglyLinkedListNode next;
+  public SinglyLinkedListNode(int nodeData) {
+    this.data = nodeData;
+    this.next = null;
+  }
 }
 
 public class InsertTail {
@@ -10,8 +14,7 @@ public class InsertTail {
   static SinglyLinkedListNode insertNodeAtTail(SinglyLinkedListNode head, int data) {
 
     if (head == null) {
-      head = new SinglyLinkedListNode();
-      head.data = data;
+      head = new SinglyLinkedListNode(data);
       head.next = null;
       return head;
     }
@@ -22,8 +25,26 @@ public class InsertTail {
 
   }
 
+  static SinglyLinkedListNode insertNodeAtHead(SinglyLinkedListNode node, int data) {
+    if (node == null) {
+      node = new SinglyLinkedListNode(data);
+      return node;
+    } else {
+      SinglyLinkedListNode newNode = new SinglyLinkedListNode(data);
+      newNode.next = node;
+      return newNode;
+    }
+
+  }
+
   public static void main(String[] args) {
-    SinglyLinkedListNode  node = insertNodeAtTail(null, 141);
-    insertNodeAtTail(node, 5);
+//    SinglyLinkedListNode  node = insertNodeAtTail(null, 141);
+//    insertNodeAtTail(node, 5);
+    SinglyLinkedListNode  node = insertNodeAtHead(null, 383);
+    node = insertNodeAtHead(node, 484);
+    node = insertNodeAtHead(node, 392);
+    node = insertNodeAtHead(node, 975);
+    node = insertNodeAtHead(node, 321);
+
   }
 }
