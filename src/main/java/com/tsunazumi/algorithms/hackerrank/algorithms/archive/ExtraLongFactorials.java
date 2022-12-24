7070ae544bf687a665b8de61af23c23b8164904b
Tom Kealamakia <tkealamakia@gmail.com>
2022-12-24 15:05:09 -0700

    Refactor package Add problem

---
+++ src/main/java/com/tsunazumi/algorithms/hackerrank/algorithms/archive/ExtraLongFactorials.java
@@ -0,0 +1,20 @@
+package com.tsunazumi.algorithms.hackerrank.algorithms.archive;
+
+import java.math.BigInteger;
+
+public class ExtraLongFactorials {
+  public static void main(String[] args) {
+   extraLongFactorials(30);
+  }
+
+  public static void extraLongFactorials(int n) {
+    BigInteger bi = new BigInteger(String.valueOf(n));
+    n = n-1;
+    while (n > 0) {
+      BigInteger val = new BigInteger(String.valueOf(n));
+      bi = bi.multiply(val);
+      n--;
+    }
+    System.out.println(bi);
+  }
+}
---
+++ src/main/java/com/tsunazumi/algorithms/hackerrank/algorithms/archive/FindDigits.java
@@ -0,0 +1,20 @@
+package com.tsunazumi.algorithms.hackerrank.algorithms.archive;
+
+public class FindDigits {
+  public static void main(String[] args) {
+    System.out.println(findDigits(10));
+  }
+
+  public static int findDigits(int n) {
+    int result = 0;
+    String num = String.valueOf(n);
+    for (Character s : num.toCharArray()) {
+      int foo = Integer.parseInt(s.toString());
+      if (foo == 0) { continue; }
+      if (n % foo == 0) {
+        result++;
+      }
+    }
+    return result;
+  }
+}
