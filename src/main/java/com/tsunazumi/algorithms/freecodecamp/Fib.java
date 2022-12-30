65707c4f696876207c6e48cecc4d774dae79d1a1
Tom Kealamakia <tkealamakia@gmail.com>
2022-12-30 14:27:31 -0700

    PrintInReverse

--- src/main/java/com/tsunazumi/algorithms/freecodecamp/Fib.java
+++ src/main/java/com/tsunazumi/algorithms/freecodecamp/Fib.java
@@ -6,13 +6,19 @@ import java.util.Map;
 public class Fib {
   public static void main(String[] args) {
     Map<Long, Long> memo = new HashMap<>();
-    System.out.println(fib(89, memo));
+    System.out.println(fib(6));
+    System.out.println(fibMemo(89, memo));
   }
 
-  public static long fib(long n, Map<Long, Long> map) {
+  public static long fib(long n) {
+    if (n <= 2) return 1;
+    return fib(n-1) + fib(n-2);
+  }
+
+  public static long fibMemo(long n, Map<Long, Long> map) {
     if (map.containsKey(n)) return map.get(n);
     if (n <= 2) return 1;
-    map.put(n, fib(n-1,map) + fib(n-2,map));
+    map.put(n, fibMemo(n-1,map) + fibMemo(n-2,map));
     return map.get(n);
   }
 }
