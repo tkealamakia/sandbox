b3a8ef60c3072710acc59890125b768afb650b23
Tom Kealamakia <tkealamakia@gmail.com>
2023-12-17 22:26:24 -0700

    Adding a file

--- src/main/java/com/tsunazumi/quiver/MaxSubarray.java
+++ src/main/java/com/tsunazumi/quiver/MaxSubarray.java
@@ -2,12 +2,20 @@ package com.tsunazumi.quiver;
 
 public class MaxSubarray {
   public static void main(String[] args) {
-    int[] input = new int[]{-2,1,-3,4,-1,2,1,-5,4};
+//    int[] input = new int[]{-2,1,-3,4,-1,2,1,-5,4};
+    int[] input = new int[]{-1};
+//    int[] input = new int[]{5,4,-1,7,8};
     int result = maxSubArray(input);
     System.out.println(result);
 
   }
   static int maxSubArray(int[] nums) {
-    return -1;
+    int currentMax = 0;
+    int maxSubArray = nums[0];
+    for (int i = 0; i < nums.length; i++) {
+      currentMax = Math.max(nums[i], nums[i] + currentMax);
+      maxSubArray = Math.max(currentMax, maxSubArray);
+    }
+    return maxSubArray;
   }
 }
