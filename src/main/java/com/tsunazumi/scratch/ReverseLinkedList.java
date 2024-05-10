f656e30043d32081f298b821128e25206dce6c91
tkealamakia <tkealamakia@gmail.com>
2024-05-09 21:31:43 -0600

    Stuff

--- src/main/java/com/tsunazumi/scratch/ReverseLinkedList.java
+++ src/main/java/com/tsunazumi/scratch/ReverseLinkedList.java
@@ -19,15 +19,6 @@ public class ReverseLinkedList {
   }
 
   public static ListNode reverse(ListNode head) {
-    ListNode prev = null;
-    ListNode current = head;
-
-    while (current != null) {
-      ListNode next = current.next;
-      current.next = prev;
-      prev = current;
-      current = next;
-    }
-    return prev;
+    return null;
   }
 }
