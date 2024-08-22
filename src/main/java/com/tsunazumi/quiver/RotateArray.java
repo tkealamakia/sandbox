6a170118e268b92950aea1091e9f082a61da6ebb
tkealamakia <tkealamakia@gmail.com>
2024-08-21 23:08:27 -0600

    Stuff

--- src/main/java/com/tsunazumi/quiver/RotateArray.java
+++ src/main/java/com/tsunazumi/quiver/RotateArray.java
@@ -4,6 +4,9 @@ public class RotateArray {
   public static void main(String[] args) {
     int[] array = {1,2,3,4};
     int[] result = rotate(array, 5);
+    for (int i : result) {
+      System.out.println(i);
+    }
   }
 
   private static int[] rotate(int[] array, int i) {
