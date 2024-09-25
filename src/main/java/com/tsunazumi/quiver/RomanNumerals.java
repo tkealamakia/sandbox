bf85175369210959c0c89d4541321e743a22b266
tkealamakia <tkealamakia@gmail.com>
2024-09-24 20:04:30 -0600

    Stuff

--- src/main/java/com/tsunazumi/quiver/RomanNumerals.java
+++ src/main/java/com/tsunazumi/quiver/RomanNumerals.java
@@ -1,9 +1,13 @@
 package com.tsunazumi.quiver;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RomanNumerals {
 
   public static void main(String[] args) {
     System.out.println(intToRoman(345));
+    System.out.println(romanToInt("CCCXLV"));
 
   }
 
@@ -19,5 +23,26 @@ public class RomanNumerals {
     String thousandPos = thousands[num / 1000];
 
     return thousandPos + hundredPos + tenPos + unitPos;
+  }
+
+  public static int romanToInt(String s) {
+    Map<Character, Integer> map = new HashMap<>();
+    map.put('I', 1);
+    map.put('V', 5);
+    map.put('X', 10);
+    map.put('L', 50);
+    map.put('C', 100);
+    map.put('D', 500);
+    map.put('M', 1000);
+
+    int result = 0;
+    for (int i = 0; i <s.length(); i++) {
+      if (i > 0 && map.get(s.charAt(i)) > map.get(s.charAt(i - 1))) {
+        result += map.get(s.charAt(i)) - 2 * map.get(s.charAt(i - 1));
+      } else {
+        result += map.get(s.charAt(i));
+      }
+    }
+    return result;
   }
 }
