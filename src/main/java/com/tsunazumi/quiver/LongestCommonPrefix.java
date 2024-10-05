99e3d665ffac1e66aa427acc078cd9cfa14a30af
tkealamakia <tkealamakia@gmail.com>
2024-10-05 11:03:18 -0600

    Stuff

--- src/main/java/com/tsunazumi/quiver/LongestCommonPrefix.java
+++ src/main/java/com/tsunazumi/quiver/LongestCommonPrefix.java
@@ -3,7 +3,7 @@ package com.tsunazumi.quiver;
 public class LongestCommonPrefix {
 
   public static void main(String[] args) {
-    String[] strings = { "flower", "flow", "flight" };
+    String[] strings = { "flow", "flower", "flight" };
     String result = findIt(strings);
     System.out.println(result);
   }
@@ -15,11 +15,14 @@ public class LongestCommonPrefix {
 
     String common = strings[0];
     for (int i = 1; i < strings.length; i++) {
+      // while current string does not contain the common string
+      // shorten it one character at a time and keep checking
       while (strings[i].indexOf(common) != 0) {
-        // shorten it down and move on
+        // if the length is eventually 0 then we don't have anything in common
         if (common.length() == 0) {
           return "";
         }
+        // shorten it down by one char and move to the next check
         common = common.substring(0, common.length() - 1);
 
       }
