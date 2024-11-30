084f8b6f40a5304443896e138b7130dd364fac46
tkealamakia <tkealamakia@gmail.com>
2024-11-30 15:06:51 -0700

    Stuff

--- src/main/java/com/tsunazumi/scratchpad/RemoveDupsFromArray.java
+++ src/main/java/com/tsunazumi/scratchpad/RemoveDupsFromArray.java
@@ -3,13 +3,13 @@ package com.tsunazumi.scratchpad;
 public class RemoveDupsFromArray {
   public static void main(String[] args) {
     int[] array = {1,2,3,4,1,2,4};
-    Integer[] result = removeDups(array);
+    int[] result = removeDups(array);
     for (Integer i : result) {
       System.out.println(i);
     }
   }
 
-  private static Integer[] removeDups(int[] array) {
+  private static int[] removeDups(int[] array) {
     return null;
   }
 }
