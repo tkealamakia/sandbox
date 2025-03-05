81722a70adcef43c095714f250dc1d912bbebbcb
Tom Kealamakia <tkealamakia@gmail.com>
2025-03-05 06:53:50 -0700

    Stuff

--- src/main/java/com/tsunazumi/quiver/ReverseLinkedList.java
+++ src/main/java/com/tsunazumi/quiver/ReverseLinkedList.java
@@ -15,13 +15,12 @@ public class ReverseLinkedList {
 
   public static ListNode reverseList(ListNode head) {
     ListNode prev = null;
-    ListNode current = head;
 
-    while (current != null) {
-      ListNode next = current.next;
-      current.next = prev;
-      prev = current;
-      current = next;
+    while (head != null) {
+      ListNode next = head.next;
+      head.next = prev;
+      prev = head;
+      head = next;
     }
     return prev;
 
