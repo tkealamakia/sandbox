d507a77d6d34233bc1dd6ba688f4c52661285932
tkealamakia <tkealamakia@gmail.com>
2025-03-22 17:18:51 -0600

    Stuff

--- src/main/java/com/tsunazumi/quiver/InsertionSort.java
+++ src/main/java/com/tsunazumi/quiver/InsertionSort.java
@@ -5,21 +5,17 @@ import java.util.Arrays;
 public class InsertionSort {
   public static void main(String[] args) {
     int[] nums = {22,4,8,-16,32};
+    // Loop through starting at second element
     for (int i = 1; i < nums.length; i++) {
-//      for (int j = i; j > 0; j--) {
-//        if (nums[j] < nums[j-1]) {
-//          int temp = nums[j];
-//          nums[j] = nums[j-1];
-//          nums[j-1] = temp;
-//        } else {
-//          break;
-//        }
-        for (int j = i; j > 0 && nums[j] < nums[j-1]; j--) {
-            int temp = nums[j];
-            nums[j] = nums[j-1];
-            nums[j-1] = temp;
-        }
+      // start at the i'th position and work backwards
+      // keep moving the i'th position number down toward the front
+      // as long as it's smaller than the one in front of it
+      for (int j = i; j > 0 && nums[j] < nums[j-1]; j--) {
+          int temp = nums[j];
+          nums[j] = nums[j-1];
+          nums[j-1] = temp;
       }
+    }
     Arrays.stream(nums).forEach(System.out::println);
   }
 }
