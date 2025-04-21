940aba169c884eb49eaa930a6748c0b9a57e9388
tkealamakia <tkealamakia@gmail.com>
2025-04-21 07:07:05 -0600

    Stuff

--- src/main/java/com/tsunazumi/quiver/RemoveDupsFromArray.java
+++ src/main/java/com/tsunazumi/quiver/RemoveDupsFromArray.java
@@ -1,13 +1,19 @@
 package com.tsunazumi.quiver;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class RemoveDupsFromArray {
   public static void main(String[] args) {
     int[] array = {1,2,3,4,1,2,4};
     Integer[] result = removeDups(array);
     for (Integer i : result) {
-      System.out.println(i);
+      System.out.print(i);
+    }
+    System.out.println();
+    int[] result2 = removeDups2(array);
+    for (Integer i : result2) {
+      System.out.print(i);
     }
   }
 
@@ -17,5 +23,15 @@ public class RemoveDupsFromArray {
       set.add(i);
     }
     return set.toArray(new Integer[0]);
+  }
+
+  private static int[] removeDups2(int[] array) {
+    return Arrays
+        .stream(array)
+        .boxed()
+        .collect(Collectors.toSet())
+        .stream()
+        .mapToInt(Integer::intValue)
+        .toArray();
   }
 }
