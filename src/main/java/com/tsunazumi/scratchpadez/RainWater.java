2460077040b69260949de2da2ba22205b0bf6852
Tom Kealamakia <kealamakia@proton.me>
2026-04-21 09:45:27 -0600

    stuff

---
+++ src/main/java/com/tsunazumi/scratchpadez/RainWater.java
@@ -0,0 +1,15 @@
+package com.tsunazumi.scratchpadez;
+
+public class RainWater {
+  public static void main(String[] args) {
+    int [] height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+    int result = trap(height);
+    System.out.println(result);
+  }
+
+  private static int trap(int[] height) {
+    return -1;
+  }
+
+
+}
