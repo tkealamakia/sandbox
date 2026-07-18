88df6e048587fca6ac677a0ff595c6af04d637fb
Tom Kealamakia <kealamakia@proton.me>
2026-07-18 10:13:56 -0600

    stuff

--- src/main/java/com/tsunazumi/scratchpadez/CanSum.java
+++ src/main/java/com/tsunazumi/scratchpadez/CanSum.java
@@ -4,12 +4,13 @@ import java.util.*;
 
 public class CanSum {
   public static void main(String[] args) {
-    int n = 7;
     List<Integer> list = Arrays.asList(7,14);
 //    List<Integer> list = Arrays.asList(2,3,1,2,4,3);
     List<Integer> result = new ArrayList<>();
     Map<Integer, Boolean> memo = new HashMap<>();
     System.out.println(canSum(300, list, memo));
+    // true result
+//    System.out.println(canSum(994, list, memo));
   }
 
   static boolean canSum(int targetSum, List<Integer> list, Map<Integer, Boolean> memo) {
